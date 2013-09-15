5951717a1bcb1ea3da1ffc39914dc9ab6470bde7
Jonathan Tsang <chewymeister88@googlemail.com>
2013-09-15 23:48:46 +0100

    Cannot get stubs to work. Also do not have timestamp. Final Test Submission

--- task_2/lib/takeaway.rb
+++ task_2/lib/takeaway.rb
@@ -1,26 +1,60 @@
+require 'twilio-ruby'
+
 class Takeaway
 	attr_reader :current_order
 	def initialize
 		@menu = {
-			'tom_yum_soup' => 4, 
+			'tom yum soup' => 4, 
 			'kung pow chicken' => 6, 
-			'hamburger' => 3
+			'hamburger' => 3,
 			'coconut rice' => 3
 		}
 		@current_order = {}
+		@sub_total = 0
+		@message = ''
 	end
 
 	def list_price menu_item
-		@menu[menu_item.to_sym]
+		@menu[menu_item]
 	end
 
 	def takes_order menu_item,quantity
 		@current_order[menu_item] = quantity
 	end
 
-	def list_order 
+	def list_current_order
+		@current_order
 	end
 
+	def current_order_subtotal
+		@current_order.each do |item, quantity|
+			price = list_price(item) * quantity
+			@sub_total += price
+		end
+		@sub_total
+	end
 
+	def place_the_order
+		@current_order.each do |item, quantity|
+			@message += "You ordered #{quantity} portions of #{item}"
+		end
+		@message += " Your sub total comes to £#{current_order_subtotal}.00."
+
+		send_text my_twilio_client
+	end
+
+	def my_twilio_client
+		account_sid = 'ACfc7164d64728e5bbc53a256c8f1abe77'
+		auth_token = 'dfc72f20a828d8fb57cbc87f9291d2be'
+		client = Twilio::REST::Client.new(account_sid, auth_token)
+	end
+
+	def send_text client
+		
+		client.account.sms.messages.create(
+		  :from => '+441904500952',
+		  :to => '+447735989592',
+		  :body => @message)
+	end
 
 end
--- task_2/spec/takeaway_spec.rb
+++ task_2/spec/takeaway_spec.rb
@@ -3,22 +3,57 @@ describe Takeaway do
 	let(:takeaway) {Takeaway.new}
 
 	it 'lists the dishes with the prices' do
-		expect(takeaway.list_price 'tom_yum_soup' ).to eq 4
+		expect(takeaway.list_price 'tom yum soup' ).to eq 4
 	end
 
 	it 'can take an order' do
-		takeaway.takes_order 'burger',2
-		expect(takeaway.current_order).to include "burger" =>2 
+		takeaway.takes_order 'hamburger',2 
+
+		expect(takeaway.current_order).to include "hamburger" =>2 
 	end
 
-	it 'can list the name of all the order I have made so far' do
-		takeaway.takes_order 'burger',2
-		takeaway.takes_order 'tom_yum_soup',1
+	it 'can list the name of all the orders the customer has made so far' do
+		takeaway.takes_order 'hamburger',2 
+		takeaway.takes_order 'tom yum soup',3 
 
-		expect
+		expect(takeaway.list_current_order).to include('hamburger' => 2,'tom yum soup' => 3)
+	end
+
+	it 'can calculate the total price of the current order' do
+		takeaway.takes_order 'hamburger',2 
+		takeaway.takes_order 'tom yum soup',3 
+
+		expect(takeaway.current_order_subtotal).to eq 18
+	end
+
+	it 'can calculate the total price of the current order' do
+		takeaway.takes_order 'kung pow chicken',3
+		takeaway.takes_order 'coconut rice',3
+		takeaway.takes_order 'hamburger',1
+
+		expect(takeaway.current_order_subtotal).to eq 30
 	end
 
 
+	xit 'can send a text message' do
+		# account_sid = 'AC9bd6d3204795d4bcbc0525c9a868fa60'
+		# authentication_token = 'b340e7741f62190d13ef9f36e34a7974'
+		# client = Twilio::REST::Client.new(account_sid, authentication_token)
+		# client.stub(:sms,:messages)
+		client = double client
+		# client.stub(:account).with(account_sid,authentication_token)
+		# client.stub_chain(:sms,:messages,:create).with(:from => "+15005550006", :to => "+44773598959", :body => "hello there would you like a cookie")
+		# client = double :client
+		client.stub_chain(:account,:sms,:messages,:create)#,:from=>'+15005550006',:to =>'+447735989592',:body=>'who are you')
+		takeaway.send_text client
+	end
 
+#========================================================================
+# Sends correct message but cannot get stubs to work.
+#========================================================================
 
+	xit 'can place the order by listing dishes' do
+		takeaway.takes_order 'tom yum soup',3
+		takeaway.place_the_order
+	end
 end
